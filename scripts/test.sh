a429f49284662b16380353769fe2aa27cce45b86
VoiceOfCoins <34265595+VoiceOfCoins@users.noreply.github.com>
2017-12-11 02:45:10 +0400

    Scripts update

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,29 +3,8 @@
 # Exit script as soon as a command fails.
 set -o errexit
 
-# Executes cleanup function at script exit.
-trap cleanup EXIT
-
-cleanup() {
-  # Kill the testrpc instance that we started (if we started one and if it's still running).
-  if [ -n "$ganache-cli_pid" ] && ps -p $ganache-cli_pid > /dev/null; then
-    kill -9 $ganache-cli_pid
-  fi
-}
-
-if [ "$SOLIDITY_COVERAGE" = true ]; then
-  ganache-cli_port=8555
-else
-  ganache-cli_port=8545
-fi
-
-ganache-cli_running() {
-  nc -z localhost "$ganache-cli_port"
-}
-
-start_ganache-cli() {
-  # We define 10 accounts with balance 1M ether, needed for high-value tests.
-  local accounts=(
+# We define 10 accounts with balance 1M ether, needed for high-value tests.
+local accounts=(
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200,1000000000000000000000000"
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501201,1000000000000000000000000"
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501202,1000000000000000000000000"
@@ -36,30 +15,10 @@ start_ganache-cli() {
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501207,1000000000000000000000000"
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501208,1000000000000000000000000"
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501209,1000000000000000000000000"
-  )
-
-  if [ "$SOLIDITY_COVERAGE" = true ]; then
-    node_modules/.bin/ganache-cli --gasLimit 0xfffffffffff --port "$ganache-cli_port" "${accounts[@]}" > /dev/null &
-  else
-    node_modules/.bin/ganache-cli --gasLimit 0xfffffffffff "${accounts[@]}" > /dev/null &
-  fi
-
-  ganache-cli_pid=$!
-}
-
-if ganache-cli_running; then
-  echo "Using existing ganache-cli instance"
-else
-  echo "Starting our own ganache-cli instance"
-  start_ganache-cli
-fi
-
-if [ "$SOLIDITY_COVERAGE" = true ]; then
-  node_modules/.bin/solidity-coverage
-
-  if [ "$CONTINUOUS_INTEGRATION" = true ]; then
-    cat coverage/lcov.info | node_modules/.bin/coveralls
-  fi
-else
-  node_modules/.bin/truffle test "$@"
-fi
+)
+  
+node_modules/.bin/ganache-cli --gasLimit 0xfffffffffff "${accounts[@]}" > /dev/null &
+  
+node_modules/.bin/truffle test
+  
+ 
