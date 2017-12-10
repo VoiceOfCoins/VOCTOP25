3b25ed03169ec3805eb78443cf4a980d0297e4d6
VoiceOfCoins <34265595+VoiceOfCoins@users.noreply.github.com>
2017-12-11 03:53:12 +0400

    Coverage update

--- truffle-config.js
+++ truffle-config.js
@@ -5,5 +5,12 @@ module.exports = {
       port: 8545,
       network_id: '*', // eslint-disable-line camelcase
     },
+    coverage: {
+      host: 'localhost',
+      network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    },
   },
 };
